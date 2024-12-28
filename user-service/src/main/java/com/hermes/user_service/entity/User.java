4ecda7d0c3ba2796fb0088212953647b8e7692a1
J0aoPaulo <contato.joaopaulodeveloper@gmail.com>
2024-12-28 00:36:01 -0300

    feat: user service entity and databse creation

--- user-service/src/main/java/com/hermes/user_service/entity/User.java
+++ user-service/src/main/java/com/hermes/user_service/entity/User.java
@@ -1,86 +1,36 @@
 package com.hermes.user_service.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "users")
+@RequiredArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Getter
+@Setter
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private Long id;
+    private String id;
 
     @Column(nullable = false)
+    @NonNull
     private String username;
 
     @Column(nullable = false)
+    @NonNull
     private String password;
 
     @Column(nullable = false)
+    @NonNull
     private String email;
 
     @Enumerated(EnumType.STRING)
+    @NonNull
     private Role role;
 
     private Boolean active = true;
-
-    public User() {
-
-    }
-
-    public User(String username, String password, String email, Role role, Boolean active) {
-        this.username = username;
-        this.password = password;
-        this.email = email;
-        this.role = role;
-        this.active = active;
-    }
-
-    public Boolean getActive() {
-        return active;
-    }
-
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
