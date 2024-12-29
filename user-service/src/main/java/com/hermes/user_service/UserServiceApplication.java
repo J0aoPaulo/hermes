2b7bb98fcc077718fe5feaa672053be6c011a011
J0aoPaulo <contato.joaopaulodeveloper@gmail.com>
2024-12-28 23:59:14 -0300

    fix: minor changes

--- user-service/src/main/java/com/hermes/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/hermes/user_service/UserServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableDiscoveryClient
+@SpringBootApplication
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
