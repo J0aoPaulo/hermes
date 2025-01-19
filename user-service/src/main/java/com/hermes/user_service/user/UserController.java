549dc89726db40e1ad42d8d66108d0ae8daeed04
J0aoPaulo <contato.joaopaulodeveloper@gmail.com>
2025-01-19 18:37:26 -0300

    fix: api gateway registration in discovery service

--- user-service/src/main/java/com/hermes/user_service/user/UserController.java
+++ user-service/src/main/java/com/hermes/user_service/user/UserController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1/users")
 public class UserController {
 
     private final UserService service;
@@ -28,7 +28,7 @@ public class UserController {
     public ResponseEntity<Void> createCustomer(@RequestBody @Valid CreateUserRequest request) {
         var userId = service.createUser(request, Role.CUSTOMER);
 
-        return ResponseEntity.created(URI.create("/api/v1/user/customer/" + userId)).build();
+        return ResponseEntity.created(URI.create("/api/v1/users/customer/" + userId)).build();
     }
 
     @PostMapping("/technician")
